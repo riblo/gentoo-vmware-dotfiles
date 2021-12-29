8051b63c7eae752851abba7703630658673129cd
Riccardo Bloise <r1cc4rd0b@gmail.com>
2021-12-29 20:37:47 +0100

    added sync.sh

--- sync.sh
+++ sync.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # home
-cp -vrf ~/.bash_logout home/bash_logout
-cp -vrf ~/.bash_profile home/bash_profile
-cp -vrf ~/.bashrc home/bashrc
-cp -vrf ~/.blerc home/blerc
-cp -vrf ~/.config/cava/config home/config/cava/config
-cp -vrf ~/.config/htop/htoprc home/config/htop/htoprc
-cp -vrf ~/.config/starship.toml home/config/starship.toml
-cp -vrf ~/.config/terminator/config home/config/terminator/config
-#cp -vrf ~/.gitconfig home/gitconfig
-cp -vrf ~/.inputrc home/inputrc
+cp -vf ~/.bash_logout home/bash_logout
+cp -vf ~/.bash_profile home/bash_profile
+cp -vf ~/.bashrc home/bashrc
+cp -vf ~/.blerc home/blerc
+cp -vf ~/.config/cava/config home/config/cava/config
+cp -vf ~/.config/htop/htoprc home/config/htop/htoprc
+cp -vf ~/.config/starship.toml home/config/starship.toml
+cp -vf ~/.config/terminator/config home/config/terminator/config
+cp -vf ~/.inputrc home/inputrc
+#cp -vf ~/.gitconfig home/gitconfig
 
 # etc
-cp -vrf /etc/dracut.conf etc/dracut.conf
-cp -vrf /etc/portage/make.conf etc/portage/make.conf
-cp -vrf /etc/portage/package.* etc/portage/
+cp -vf /etc/dracut.conf etc/dracut.conf
+cp -vf /etc/portage/make.conf etc/portage/make.conf
+cp -vf /etc/portage/package.* etc/portage/
 cp -vrf /etc/portage/env etc/portage/
